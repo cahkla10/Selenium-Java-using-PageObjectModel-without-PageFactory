14359e46f95a33169e1e3585d32834fac7642298
Lingga <32485683+cahkla10@users.noreply.github.com>
2022-12-14 10:31:19 +0700

    update ExplorePage

--- src/test/java/pages/ExplorePage.java
+++ src/test/java/pages/ExplorePage.java
@@ -14,24 +14,12 @@ public class ExplorePage {
     Common common = new Common();
 
     public void clickContentGroup(String contentGroupName) throws InterruptedException {
-        List<WebElement> contentGroups = BrowserPool.getBrowserInstance().findElements(By.xpath(contentGroup));
-        System.out.println("Clicked Content Group is " + contentGroupName);
-        for(WebElement contentGroupIdx : contentGroups){
-            if(contentGroupIdx.getText().contains(contentGroupName)){
-                contentGroupIdx.click();
-                System.out.println(contentGroupIdx.getText() + "Clicked!");
-                break;
-            }
-        }
+        common.findXpathAndClick(contentGroup, contentGroupName);
         common.delay(delay);
     }
 
     public void readOneOfContentGroup() throws InterruptedException{
-        List<WebElement> contentGroups = BrowserPool.getBrowserInstance().findElements(By.xpath(contentGroup));
-        System.out.println("Explore Page is opened.");
-        for(WebElement cGroups : contentGroups){
-            System.out.println("Cek Content Group is: " + cGroups.getText());
-        }
+        common.findXpathAndRead(contentGroup,"Explore Page is opened.");
         common.delay(delay);
     }
 }
