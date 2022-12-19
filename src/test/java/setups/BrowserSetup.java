fa6df7968ee74d0abf11d45bd688442ec8ac1cb6
Lingga <32485683+cahkla10@users.noreply.github.com>
2022-12-19 15:43:40 +0700

    Update and rename ChromeSetup.java to BrowserSetup.java

--- src/test/java/setups/BrowserSetup.java
+++ src/test/java/setups/BrowserSetup.java
@@ -3,19 +3,19 @@ package setups;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class ChromeSetup extends ChromePool{
-    ChromeDriver browser;
+public class BrowserSetup extends BrowserPool {
+    ChromeDriver chrome;
 
     protected void startChrome() {
 //        System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver");
         System.setProperty("webdriver.chrome.driver","D:\\OTOMESEN\\saucedemo\\src\\test\\resources\\chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
-        browser = new ChromeDriver(options);
-        setChromeInstance(browser);
+        chrome = new ChromeDriver(options);
+        setChromeInstance(chrome);
     }
 
     protected void stopChrome() {
-        browser.close();
+        chrome.close();
     }
 }
