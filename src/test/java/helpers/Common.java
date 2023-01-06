f28fc08cd92cf9ae60c7eeff692925fb492b985a
Cahkla10 <32485683+cahkla10@users.noreply.github.com>
2023-01-06 10:52:04 +0700

    Update Common.java

--- src/test/java/helpers/Common.java
+++ src/test/java/helpers/Common.java
@@ -50,6 +50,7 @@ public class Common {
     public void verifyText(By element, String expected){
         List<WebElement> list = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(element));
         if(list.size() >= 1){
+            System.out.println("Expected: " + expected);
             assertEquals(expected,list.get(0).getText());
         }
     }
