c97b36e761d658ce912f607f567c49c4507c5ff6
Cahkla10 <32485683+cahkla10@users.noreply.github.com>
2023-01-06 10:52:27 +0700

    Update Common.java

--- src/test/java/helpers/Common.java
+++ src/test/java/helpers/Common.java
@@ -32,7 +32,7 @@ public class Common {
 
     public void findAndRead(By element, String outputText){
         List<WebElement> list = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(element));
-        if(list.size() >= 1){
+        if(list.size() > 0){
             System.out.println(outputText);
         }
     }
@@ -49,7 +49,7 @@ public class Common {
 
     public void verifyText(By element, String expected){
         List<WebElement> list = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(element));
-        if(list.size() >= 1){
+        if(list.size() > 0){
             System.out.println("Expected: " + expected);
             assertEquals(expected,list.get(0).getText());
         }
